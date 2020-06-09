7a19f4ea8289be339ad1367e119da5ee30297bb1
Nikola Urešová <nikola.uresova@gmail.com>
2020-06-09 11:38:56 +0200

    update

--- ukol.js
+++ ukol.js
@@ -1,33 +1,25 @@
 'use strict'
 
 let osoba1 = {
-  jmeno: 'Alena',
-  uspory: 43000
+    jmeno: 'Alena',
+    uspory: 100000
 };
 let osoba2 = {
-  jmeno: 'Karolína',
-  uspory: 68000
+    jmeno: 'Karolína',
+    uspory: 1000
 };
 
 const holiday = () => {
-  if (osoba1.uspory + osoba2.uspory <= 100000) {
-      console.log('Kamarádky nemají dost peněz, nikam nepojedou.');
-      console.log(`Kamarádky musí našetřit ${100000 - osoba1.uspory - osoba2.uspory} Kč.`)
-      if (osoba1.uspory < 50000) {
-          console.log(`${osoba1.jmeno} musí ušetřit ${50000 - osoba1.uspory}, aby mohla jet na dovolenou.`)
-      };
-      if (osoba2.uspory < 50000) {
-          console.log(`${osoba2.jmeno} musí ještě ušetřit ${50000 - osoba2.uspory}, aby mohla jet na dovolenou.`)
-      };
-  } else if (osoba1.uspory <= 50000 && osoba2.uspory >= 500000) {
-      console.log('Kamarádky mají dost peněz, mohou odjet.');
-  } else {
-      if (osoba1.uspory <= 500000) {
-          console.log(`${osoba1.jmeno} musí ještě zaplatit ${50000 - osoba1.uspory} Kč.`)
-      } else {
-          console.log(`${osoba2.jmeno} musí ještě zaplatit ${50000 - osoba2.uspory} Kč.`)
-      }
-  }
-}
+    if (osoba1.uspory + osoba2.uspory >= 100000) {
+        if (osoba1.uspory >= 50000 && osoba2.uspory >= 50000) {
+            return 'Holky mohou jet na Havaj, mají naspořeno dost peněz.';
+        } if (osoba1.uspory >= 50000 && osoba2.uspory <= 50000) {
+            return `${osoba1.jmeno} má našetřeno dostatek peněz. ${osoba2.jmeno} ještě chybí našetřit ${50000 - osoba2.uspory} Kč. `;
+        } if (osoba2.uspory >= 50000 && osoba1.uspory <= 50000) {
+            return `${osoba2.jmeno} má našetřeno dostatek peněz. ${osoba1.jmeno} ještě chybí našetřit ${50000 - osoba1.uspory} Kč.`;
+        }
+    }
+    return `Holky musí našetřit ${100000 - osoba1.uspory - osoba2.uspory} Kč. ${osoba1.jmeno} ještě chybí našetřit ${50000 - osoba1.uspory} Kč, ${osoba2.jmeno} ještě chybí našetřit ${50000 - osoba1.uspory} Kč.`;
+};
 
-holiday();
+console.log(holiday());
